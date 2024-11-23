33dc14b2ff2b30f6c9cf175e886d4db375b75f4c
cache18 <cache18@gmail.com>
2024-11-23 23:09:36 +0100

    assignment - add user

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -21,6 +21,15 @@ public class BeerControllerIT extends BaseIT {
     }
 
     @Test
+    void testScottUser() throws Exception{
+        mockMvc.perform(get("/beers/new").with(httpBasic("scott", "tiger")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+
+    }
+
+    @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
                 .andExpect(status().isOk())
