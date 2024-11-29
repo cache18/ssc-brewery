74c8a93980442551e527ff7352f5d69865da7420
cache18 <cache18@gmail.com>
2024-11-29 14:48:30 +0100

    08 - UserDetails config

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -64,22 +64,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("spring")
-                .password("{bcrypt}$2a$10$npfj68GW/fnQnTF3T6FcNe/43aBx5v9RHUI8Lo33yUEhZbjz30cDO")
-                .roles("ADMIN")
-                .and()
-                .withUser("user")
-                .password("{sha256}60c7a3719e5179d4083c2c5781ee24f6d232ffa59d8d2ac3d973aaf323493b11cf0f0dd4c6c70b62")
-                .roles("USER");
-
-        auth.inMemoryAuthentication()
-                .withUser("scott")
-                .password("{bcrypt10}$2a$10$MpnfnP9BZNucsDAMlwgw5.jII2VJkooqdw9rNZzCpp6huWebighJ2")
-                .roles("CUSTOMER");
-    }
+    //do not needed anymore as PasswordEncoder and UserDetailsService are beans and SB can use them
+    //to autoconfig for use
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("spring")
+//                .password("{bcrypt}$2a$10$npfj68GW/fnQnTF3T6FcNe/43aBx5v9RHUI8Lo33yUEhZbjz30cDO")
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("user")
+//                .password("{sha256}60c7a3719e5179d4083c2c5781ee24f6d232ffa59d8d2ac3d973aaf323493b11cf0f0dd4c6c70b62")
+//                .roles("USER");
+//
+//        auth.inMemoryAuthentication()
+//                .withUser("scott")
+//                .password("{bcrypt10}$2a$10$MpnfnP9BZNucsDAMlwgw5.jII2VJkooqdw9rNZzCpp6huWebighJ2")
+//                .roles("CUSTOMER");
+//    }
 
     //    @Override
 //    @Bean
--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -4,12 +4,14 @@ import guru.sfg.brewery.domain.security.Authority;
 import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Collection;
@@ -17,14 +19,18 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JpaUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    @Transactional //or eager fetching on user.authorities
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.debug("Getting user info via JPA");
+
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User name " + username + " not found"));
 
