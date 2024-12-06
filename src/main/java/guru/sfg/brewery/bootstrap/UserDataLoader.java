50788676f81bffbbadbb58904217e356852dddfe
cache18 <cache18@gmail.com>
2024-12-06 10:24:53 +0100

    10 - add transactional to save all entities in one transaction

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -80,6 +81,7 @@ public class UserDataLoader implements CommandLineRunner {
         log.debug("Loaded users: {}", userRepository.count());
     }
 
+    @Transactional
     @Override
     public void run(String... args) {
         if (authorityRepository.count() == 0) {
