7edd62e724099d4f3c0ca568b5e7ff2989739cf9
cache18 <cache18@gmail.com>
2025-04-03 23:32:23 +0200

    11 - using AuthenticationPrinciple annotation

---
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderControllerV2.java
@@ -0,0 +1,70 @@
+/*
+ *  Copyright 2019 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.security.perms.BeerOrderReadPermissionV2;
+import guru.sfg.brewery.services.BeerOrderService;
+import guru.sfg.brewery.web.model.BeerOrderDto;
+import guru.sfg.brewery.web.model.BeerOrderPagedList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v2/orders/")
+public class BeerOrderControllerV2 {
+
+    private static final Integer DEFAULT_PAGE_NUMBER = 0;
+    private static final Integer DEFAULT_PAGE_SIZE = 25;
+
+    private final BeerOrderService beerOrderService;
+
+    @BeerOrderReadPermissionV2
+    @GetMapping
+    public BeerOrderPagedList listOrders(@AuthenticationPrincipal User user,
+                                         @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                         @RequestParam(value = "pageSize", required = false) Integer pageSize){
+
+        if (pageNumber == null || pageNumber < 0){
+            pageNumber = DEFAULT_PAGE_NUMBER;
+        }
+
+        if (pageSize == null || pageSize < 1) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+        if (user.getCustomer() != null) {
+            return beerOrderService.listOrders(user.getCustomer().getId(), PageRequest.of(pageNumber, pageSize));
+        } else {
+            return beerOrderService.listOrders(PageRequest.of(pageNumber, pageSize));
+        }
+    }
+
+    @BeerOrderReadPermissionV2
+    @GetMapping("orders/{orderId}")
+    public BeerOrderDto getOrder(@PathVariable("orderId") UUID orderId){
+
+        return null;
+        //  return beerOrderService.getOrderById(orderId);
+    }
+}
